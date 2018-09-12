e8bdb5605d475d1382da04cf2095bf52b116c4ee
SongHuan <112597649@qq.com>
2018-09-12 20:32:11 +0800

    [dev] oo

--- src/main/java/com/sh/yunpian/MesSendTest.java
+++ src/main/java/com/sh/yunpian/MesSendTest.java
@@ -3,7 +3,9 @@ package com.sh.yunpian;
 import com.sh.util.ApacheHttpUtil;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,14 +40,24 @@ public class MesSendTest {
     private static String ENCODING = "UTF-8";
 
     public static void main(String[] args) {
-        String apiKey = "a8209649053471054ec2d8a33bc187e0";
+        String apiKey = "1ac913e340f99d64758a4c34db869c9a";
+//        String apiKey = "a8209649053471054ec2d8a33bc187e0";
         // 发送的手机号
         String mobile = "18281606652";
         /** 查账户信息条用示例 */
         System.out.println(MesSendTest.getUserInfo(apiKey));
-        String text = "【沱江一日游】欢迎来到沱江一日游";
-        String result = sendSms(apiKey, text, mobile);
-        System.out.println("返回结果：" + result);
+        String text = null;
+        try {
+            text = URLEncoder.encode("#value#", "UTF-8") + "=" +
+                    URLEncoder.encode("设备1", "UTF-8") + "&" +
+                    URLEncoder.encode("#device#", "UTF-8") + "=" + URLEncoder.encode("1234567", "UTF-8");
+//            text = URLEncoder.encode("#code#", "UTF-8") + "=" +
+//                    URLEncoder.encode("123456", "UTF-8");
+            String result = sendSms(apiKey, text, mobile);
+            System.out.println("返回结果：" + result);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -61,11 +73,12 @@ public class MesSendTest {
         return ApacheHttpUtil.post(URI_GET_USER_INFO, params);
     }
 
-    public static String sendSms(String apikey, String text, String mobile) {
+    public static String sendSms(String apikey, String tplValue, String mobile) {
         Map <String, String> params = new HashMap <> ();
         params.put("apikey", apikey);
-        params.put("text", text);
+        params.put("tpl_value", tplValue);
+        params.put("tpl_id", "2475088");
         params.put("mobile", mobile);
-        return ApacheHttpUtil.post(URI_SEND_SMS, params);
+        return ApacheHttpUtil.post(URI_TPL_SEND_SMS, params);
     }
 }
